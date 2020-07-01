6f70cdcc33a7a90b6cdceedc5f1ba62a30974526
Ben Dwyer <ben@scruffian.com>
2020-07-01 16:59:10 +0100

    Add styles for the modal

--- apps/wpcom-block-editor/src/wpcom/editor.js
+++ apps/wpcom-block-editor/src/wpcom/editor.js
@@ -17,6 +17,11 @@ import './features/reorder-block-categories';
 import './features/tracking';
 import './features/use-classic-block-guide';
 
+/**
+ * Style dependencies
+ */
+import './editor.scss';
+
 import InserterMenuTrackingEvent from './features/tracking/wpcom-inserter-menu-search-term';
 
 registerPlugin( 'track-inserter-menu-events', {
